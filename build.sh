9067d0e619a5f3cd005f1c97efca5586f73f1a7e
denvercoder98 <45121053+denvercoder98@users.noreply.github.com>
2018-12-06 11:48:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-CUCUMBER_DIR=lib/cucumber-cpp-0.5
-if [ ! -d "$CUCUMBER_DIR/install" ]; then
-	echo "Building cucumber-cpp"
-	pushd . > /dev/null
-	cd $CUCUMBER_DIR
-	CUCUMBER_INSTALL_DIR="$(pwd)/install"
-	cmake -E make_directory build
-	cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DCMAKE_INSTALL_PREFIX=$CUCUMBER_INSTALL_DIR ..
-	cmake --build build
-	cmake --build build --target install
-	popd > /dev/null
-else
-	echo "cucumber-cpp already built"
-fi
+#CUCUMBER_DIR=lib/cucumber-cpp-0.5
+#if [ ! -d "$CUCUMBER_DIR/install" ]; then
+#	echo "Building cucumber-cpp"
+#	pushd . > /dev/null
+#	cd $CUCUMBER_DIR
+#	CUCUMBER_INSTALL_DIR="$(pwd)/install"
+#	cmake -E make_directory build
+#	cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DCMAKE_INSTALL_PREFIX=$CUCUMBER_INSTALL_DIR ..
+#	cmake --build build
+#	cmake --build build --target install
+#	popd > /dev/null
+#else
+#	echo "cucumber-cpp already built"
+#fi
 
 GOOGLETEST_DIR=lib/googletest-release-1.8.1
 if [ ! -d "$GOOGLETEST_DIR/install" ]; then
