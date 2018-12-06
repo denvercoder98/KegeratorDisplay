a2d22c63a6427a4403d9f57685092295165d5046
denvercoder98 <45121053+denvercoder98@users.noreply.github.com>
2018-12-06 10:30:43 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 CUCUMBER_DIR=lib/cucumber-cpp-0.5
 if [ ! -d "$CUCUMBER_DIR/build" ]; then
 	echo "Building cucumber-cpp"
-	pushd . > /dev/null
-	cd $CUCUMBER_DIR
-	CUCUMBER_DIR_ABSOLUTE="$(pwd)/build"
-	cmake -E make_directory build
-	cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DCMAKE_INSTALL_PREFIX=$CUCUMBER_DIR_ABSOLUTE ..
-	cmake --build build
-	cmake --build build --target install
-	popd > /dev/null
+#	pushd . > /dev/null
+#	cd $CUCUMBER_DIR
+#	CUCUMBER_DIR_ABSOLUTE="$(pwd)/build"
+#	cmake -E make_directory build
+#	cmake -E chdir build cmake -DCUKE_ENABLE_EXAMPLES=on -DCMAKE_INSTALL_PREFIX=$CUCUMBER_DIR_ABSOLUTE ..
+#	cmake --build build
+#	cmake --build build --target install
+#	popd > /dev/null
 else
 	echo "cucumber-cpp already built"
 fi
@@ -18,13 +18,13 @@ fi
 GOOGLETEST_DIR=lib/googletest-release-1.8.1
 if [ ! -d "$GOOGLETEST_DIR/build" ]; then
 	echo "Building googletest"
-	pushd . > /dev/null
-	cd $GOOGLETEST_DIR
-	GOOGLETEST_DIR_ABSOLUTE="$(pwd)/build"
-	cmake -E make_directory build
-	cmake -E chdir build cmake ..
-	cmake --build build
-	popd > /dev/null
+#	pushd . > /dev/null
+#	cd $GOOGLETEST_DIR
+#	GOOGLETEST_DIR_ABSOLUTE="$(pwd)/build"
+#	cmake -E make_directory build
+#	cmake -E chdir build cmake ..
+#	cmake --build build
+#	popd > /dev/null
 else
 	echo "googletest already built"
 fi
