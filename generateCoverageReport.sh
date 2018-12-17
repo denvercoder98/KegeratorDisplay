3fc863c0a1ba0ddf268be99178afd08598e95ca4
krja <kr@kj.se>
2018-12-17 15:29:25 +0100

    Removed view from unit test coverage report since they are considered humble and will never be tested

--- generateCoverageReport.sh
+++ generateCoverageReport.sh
@@ -10,4 +10,5 @@ lcov -c -d CMakeFiles -o $COVERAGE_DIR/app_cov.info
 lcov -a $COVERAGE_DIR/app_base.info -a $COVERAGE_DIR/app_cov.info -o $COVERAGE_DIR/app_total.info
 lcov -e $COVERAGE_DIR/app_total.info *main* -o $COVERAGE_DIR/app_main_src_only.info
 lcov --remove $COVERAGE_DIR/app_main_src_only.info Main.cpp -o $COVERAGE_DIR/app_src_only.info
-genhtml $COVERAGE_DIR/app_src_only.info -o $COVERAGE_DIR/out
+lcov --remove $COVERAGE_DIR/app_src_only.info view/* -o $COVERAGE_DIR/app_src_only_no_view.info
+genhtml $COVERAGE_DIR/app_src_only_no_view.info -o $COVERAGE_DIR/out
